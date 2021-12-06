14617f97947709db5bf03bf420f5576af23a08cd
romaRacoon <roma1808@inbox.ru>
2021-12-07 00:51:29 +0300

    1

---
+++ Практическая 6/Student/SortingStudentsByGPA.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+import java.util.Comparator;
+
+public class SortingStudentsByGPA implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        if(!(o1 instanceof Student && o2 instanceof Student))
+            throw new IllegalArgumentException("Объекты не являются студентами!");
+
+        return ((Student) o1).getGPA() - ((Student) o2).getGPA();
+    }
+}
--- Практическая 6/Задание 1/Test.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Test {
-    public static void main(String[] args) {
-        Student student = new Student();
-        student.SelectionSort();
-        student.PrintScr();
-    }
-}
