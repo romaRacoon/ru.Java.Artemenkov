861512ec4ea7482675558351267971aa5258bb1d
romaRacoon <roma1808@inbox.ru>
2021-12-07 00:34:13 +0300

    version 1.8

---
+++ Практическая 6/Задание 1/Test.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.SelectionSort();
+        student.PrintScr();
+    }
+}
